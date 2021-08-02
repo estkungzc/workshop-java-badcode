889299a22e92996a59e9c786824d60155d896939
KBANKNET\chairat.p <chairat.p@kbtg.tech>
2021-08-02 17:21:23 +0700

    refactor(RegisterBusiness): introduce extract method, return early for valid speaker input

--- src/main/java/badcode/RegisterBusiness.java
+++ src/main/java/badcode/RegisterBusiness.java
@@ -4,37 +4,33 @@ import java.util.Arrays;
 
 public class RegisterBusiness {
 
+    String[] domains = {"gmail.com", "live.com"};
+
     public Integer register(SpeakerRepository repository, Speaker speaker) {
         Integer speakerId;
-        String[] domains = {"gmail.com", "live.com"};
 
-        if (speaker.getFirstName() != null && !speaker.getFirstName().trim().equals("")) {
-            if (speaker.getLastName() != null && !speaker.getLastName().trim().equals("")) {
-                if (speaker.getEmail() != null && !speaker.getEmail().trim().equals("")) {
-                    // Your Tasks ...
-                    String emailDomain = getEmailDomain(speaker.getEmail()); // Avoid ArrayIndexOutOfBound
-                    if (Arrays.stream(domains).filter(it -> it.equals(emailDomain)).count() == 1) {
-                        int exp = speaker.getExp();
-                        speaker.setRegistrationFee(getFee(exp));
-                        try {
-                            speakerId = repository.saveSpeaker(speaker);
-                        } catch (Exception exception) {
-                            throw new SaveSpeakerException("Can't save a speaker.");
-                        }
-                    } else {
-                        throw new SpeakerDoesntMeetRequirementsException("Speaker doesn't meet our standard rules.");
-                    }
-                } else {
-                    throw new ArgumentNullException("Email is required.");
-                }
-            } else {
-                throw new ArgumentNullException("Last name is required.");
+        if (!isFieldExist(speaker.getFirstName())) throw new ArgumentNullException("First name is required.");
+        if (!isFieldExist(speaker.getLastName())) throw new ArgumentNullException("Last name is required.");
+        if (!isFieldExist(speaker.getEmail())) throw new ArgumentNullException("Email is required.");
+
+        String emailDomain = getEmailDomain(speaker.getEmail()); // Avoid ArrayIndexOutOfBound
+        if (Arrays.stream(domains).filter(it -> it.equals(emailDomain)).count() == 1) {
+            int exp = speaker.getExp();
+            speaker.setRegistrationFee(getFee(exp));
+            try {
+                speakerId = repository.saveSpeaker(speaker);
+            } catch (Exception exception) {
+                throw new SaveSpeakerException("Can't save a speaker.");
             }
         } else {
-            throw new ArgumentNullException("First name is required.");
+            throw new SpeakerDoesntMeetRequirementsException("Speaker doesn't meet our standard rules.");
         }
 
         return speakerId;
+    }
+
+    private boolean isFieldExist(String field) {
+        return field != null && !field.trim().equals("");
     }
 
     int getFee(int exp) {
