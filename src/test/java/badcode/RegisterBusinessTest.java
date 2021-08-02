35a3bff5a11c256970eb79fe692f1c42badc8a55
KBANKNET\chairat.p <chairat.p@kbtg.tech>
2021-08-02 15:06:14 +0700

    test(RegisterBusinessTest): add case last name is null

--- src/test/java/badcode/RegisterBusinessTest.java
+++ src/test/java/badcode/RegisterBusinessTest.java
@@ -17,4 +17,17 @@ class RegisterBusinessTest {
         );
         assertEquals("First name is required.", exception.getMessage());
     }
+
+    @Test
+    @DisplayName("Have First name but Last name is null, it should be occur exception")
+    public void case02() {
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("Foo");
+
+        Exception exception = assertThrows(ArgumentNullException.class, () ->
+                registerBusiness.register(null, speaker)
+        );
+        assertEquals("Last name is required.", exception.getMessage());
+    }
 }
